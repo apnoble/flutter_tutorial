711588cdbc14338ec05f09fabda483b6ededf6fc
anoble25 <AWGR-Alex@comcast.net>
2018-10-07 22:06:08 -0700

    add custom message based on the time and make app print out formatted date

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,16 @@ class HelloFlutterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Hello Flutter App",
       home: Scaffold(
         appBar: AppBar(
           title: Text("Title in App Bar"),
+          
         ),
         body: Home()
       ),
+      
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,11 +7,26 @@ class Home extends StatelessWidget {
           color: Colors.deepPurple,
           child: Center(
             child: Text(
-              "Hello Jen",
+              sayHello(),
               textDirection: TextDirection.ltr,
               style: TextStyle(color: Colors.white, fontSize: 36.0),
             ),
           ),
         );
+  }
+  String sayHello() {
+    String hello;
+    DateTime now = new DateTime.now();
+    int hour = now.hour;
+    if (hour < 12) {
+      hello = "Good Morning";
+    }
+    else if (hour < 18 ) {
+      hello = "Good Afternoon";
+    }
+    else {
+      hello = "Good Night";
+    }
+    return "The time is " + now.hour.toString() + ":"+ now.minute.toString().padLeft(2,'0')+" "+hello;
   }
 }
